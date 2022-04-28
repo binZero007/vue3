53adc2bc57cf959ecf83e8e5ccff025788119635
wuxiaobin <2568298744@qq.com>
2022-04-28 23:00:04 +0800

    第一次提交尝试

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,17 @@
+<!--
+ * @Author: your name
+ * @Date: 2022-04-28 21:16:31
+ * @LastEditTime: 2022-04-28 22:48:40
+ * @LastEditors: your name
+ * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ * @FilePath: \vue3\src\App.vue
+-->
 <template>
   <nav>
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
   </nav>
+<!-- 测试 -->
   <router-view/>
 </template>
 
