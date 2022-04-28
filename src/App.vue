4e3884157ce62aa1edf067e20c2ae74ebfdf26de
wuxiaobin <2568298744@qq.com>
2022-04-28 23:11:07 +0800

    测试第二次提交

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,11 @@
+<!--
+ * @Author: wuxiaobin
+ * @Date: 2022-04-28 21:16:31
+ * @LastEditTime: 2022-04-28 23:15:27
+ * @LastEditors: wuxiaobin
+ * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ * @FilePath: \vue3\src\App.vue
+-->
 <template>
   <nav>
     <router-link to="/">Home</router-link> |
