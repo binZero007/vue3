713dd0f750b76cba252740a980164fe1c9df1717
wuxiaobin <2568298744@qq.com>
2022-04-28 23:11:07 +0800

    测试第二次提交

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <!--
- * @Author: your name
+ * @Author: wuxiaobin
  * @Date: 2022-04-28 21:16:31
- * @LastEditTime: 2022-04-28 22:48:40
- * @LastEditors: your name
+ * @LastEditTime: 2022-04-28 23:04:17
+ * @LastEditors: wuxiaobin
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: \vue3\src\App.vue
 -->
@@ -11,7 +11,7 @@
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
   </nav>
-<!-- 测试 -->
+<!-- 测试-->
   <router-view/>
 </template>
 
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,3 +1,10 @@
+<!--
+ * @Author: wuxiaobin
+ * @Date: 2022-04-28 21:16:31
+ * @LastEditors: wuxiaobin
+ * @LastEditTime: 2022-04-28 23:04:55
+ * @FilePath: \vue3\src\views\HomeView.vue
+-->
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
@@ -8,7 +15,7 @@
 <script lang="ts">
 import { defineComponent } from 'vue'
 import HelloWorld from '@/components/HelloWorld.vue' // @ is an alias to /src
-
+//测试头部
 export default defineComponent({
   name: 'HomeView',
   components: {
