5bd9622ccd96029f047eeb9bf27fbbf41c936cbe
wuxiaobin <2568298744@qq.com>
2022-05-08 22:38:36 +0800

    测试git

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <!--
  * @Author: wuxiaobin
  * @Date: 2022-04-28 21:16:31
- * @LastEditTime: 2022-04-28 23:15:27
+ * @LastEditTime: 2022-05-08 22:34:31
  * @LastEditors: wuxiaobin
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: \vue3\src\App.vue
@@ -11,7 +11,7 @@
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
   </nav>
-  <router-view/>
+  <router-view />
   <!-- 测试二 -->
 </template>
 
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,25 @@
+<!--
+ * @Author: wuxiaobin
+ * @Date: 2022-04-28 21:16:31
+ * @LastEditors: wuxiaobin
+ * @LastEditTime: 2022-05-08 22:17:25
+ * @FilePath: \vue3\src\views\AboutView.vue
+-->
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <h1>{{ count }}</h1>
   </div>
 </template>
+<script lang="ts">
+import { defineComponent } from 'vue'
+export default defineComponent({
+  name: 'aboutItem',
+  setup() {
+    const count = 1
+    return {
+      count
+    }
+  }
+})
+</script>
